dfc5edc4a76ab2c424c07e81f79c39db63a6c7a1
hahn5 <jfhahn@andrew.cmu.edu>
2024-07-01 19:40:29 +0000

    fix wrong push

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import os
 
 def main():
+       
     html = '<html><head><meta http-equiv="refresh" content="0;url={url}" /></head></html>'
 
     with open('links.json') as f:
@@ -9,6 +10,11 @@ def main():
     
     if not os.path.exists('docs') and not os.path.isdir('docs'):
         os.mkdir('docs')
+    
+    # Change to your custom domain (remove if you don't have one)
+    url = "jofaha.de"
+    with open('docs/CNAME', 'w') as f:
+        f.write(url)
 
     for link in links:
         html_document = html.format(url=link['url'])
